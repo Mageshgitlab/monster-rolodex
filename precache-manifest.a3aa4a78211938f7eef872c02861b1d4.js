84c8326ea487610788d897e5817769addce6367b
Mageshgitlab <mageshsekar05@gmail.com>
2020-08-29 17:12:02 +0530

    Updates

---
+++ precache-manifest.a3aa4a78211938f7eef872c02861b1d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0cfd7a1f2d3e3e256105cc364ca3e7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "433a533aae6ed70e3cde",
+    "url": "/monster-rolodex/static/css/main.fd80797c.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/monster-rolodex/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433a533aae6ed70e3cde",
+    "url": "/monster-rolodex/static/js/main.67a0899c.chunk.js"
+  },
+  {
+    "revision": "7d7808272c82add620fd",
+    "url": "/monster-rolodex/static/js/runtime-main.feeca918.js"
+  }
+]);
